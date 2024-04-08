b09b8c81b687d19ead2764a08fcd42d3e294c47d
Jmendes <jmendes.3595@gmail.com>
2024-04-08 18:08:36 +0100

    Rejecting working

--- include/actionclient/ActionClient.h
+++ include/actionclient/ActionClient.h
@@ -7,47 +7,55 @@
 #include <rclcpp_action/rclcpp_action.hpp>
 
 
-template<typename Service>
+template<typename Action>
 class ActionClient
 {
 public:
     ActionClient(Node* parent, std::string const& service);
 
-    std::shared_future<std::shared_ptr<rclcpp_action::ClientGoalHandle<Service>>>
+    std::shared_future<std::shared_ptr<rclcpp_action::ClientGoalHandle<Action>>>
         asyncSendGoal();
 
 protected:
+
+    virtual void goalResponseCallback(
+        typename rclcpp_action::ClientGoalHandle<Action>::SharedPtr const& response) = 0;
+
+    virtual void goalResultCallback(
+        typename rclcpp_action::ClientGoalHandle<Action>::WrappedResult const& result) = 0;
+
     rclcpp::Logger getLogger();
 
-    typename Service::Goal _goal;
-    typename rclcpp_action::Client<Service>::SendGoalOptions _options;
+    typename Action::Goal _goal;
+    typename rclcpp_action::Client<Action>::SendGoalOptions _options;
 
 private:
-    typename rclcpp_action::Client<Service>::SharedPtr _client;
+    typename rclcpp_action::Client<Action>::SharedPtr _client;
 
     Node* _parent;
 };
 
 
-template<typename Service>
-ActionClient<Service>::ActionClient(Node *parent, std::string const& service)
+template<typename Action>
+ActionClient<Action>::ActionClient(Node *parent, std::string const& service)
     : _parent(parent)
-    , _client(rclcpp_action::create_client<Service>(parent, service))
+    , _client(rclcpp_action::create_client<Action>(parent, service))
 {
-
+    _options.goal_response_callback = [this](auto const& response) { goalResponseCallback(response); };
+    _options.result_callback = [this](auto const& result) { goalResultCallback(result); };
 }
 
-template<typename Service>
-std::shared_future<std::shared_ptr<rclcpp_action::ClientGoalHandle<Service>>>
-ActionClient<Service>::asyncSendGoal()
+template<typename Action>
+std::shared_future<std::shared_ptr<rclcpp_action::ClientGoalHandle<Action>>>
+ActionClient<Action>::asyncSendGoal()
 {
     _client->wait_for_action_server();
 
     return _client->async_send_goal(_goal, _options);
 }
 
-template<typename Service>
-rclcpp::Logger ActionClient<Service>::getLogger()
+template<typename Action>
+rclcpp::Logger ActionClient<Action>::getLogger()
 {
     return _parent->get_logger();
 }
